d8590b170a94c9ea385463f24d54b48eea83b848
yeonkyoung <keep.ykk@gmail.com>
2023-03-16 00:14:25 +0900

    [#2] Feat : App 컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
-import styled from 'styled-components';
+import { ChakraProvider } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+
+import instance from './apis/instance';
+import { ResponseDataType } from './common/Types';
+import Chart from './components/Chart';
+import Filter from './components/Filter';
+import { ContextAPI } from './hooks/ContextAPI';
 
 function App() {
-  return (
-    <div>
-      <StyledBox />
-    </div>
-  );
+  const [chartData, setChartData] = useState<ResponseDataType[]>();
+  const getInstance = async () => {
+    const data = await instance.get('');
+    const response: ResponseDataType[] = Object.values(data.data.response);
+    setChartData([...response]);
+  };
+  const [selectedId, setSelectedId] = useState<string>('');
+  const handleButton = (target: string) => {
+    console.log(selectedId);
+    setSelectedId(target);
+  };
+
+  useEffect(() => {
+    getInstance();
+  }, []);
+
+  if (chartData) {
+    return (
+      <div>
+        <ContextAPI.Provider value={chartData}>
+          <ChakraProvider>
+            <Filter onClick={handleButton} />
+          </ChakraProvider>
+          <Chart selectedId={selectedId} />
+        </ContextAPI.Provider>
+      </div>
+    );
+  }
+  return <div />;
 }
-
 export default App;
-
-const StyledBox = styled.div`
-  width: 300px;
-  height: 300px;
-  background-color: tomato;
-`;
