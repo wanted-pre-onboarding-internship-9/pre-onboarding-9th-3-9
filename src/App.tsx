35c972f874f98f9356626ee436205fc2ddcbe94f
yeonkyoung <keep.ykk@gmail.com>
2023-03-17 10:11:49 +0900

    [#2] Refactor : getData코드를 분리, useResponse 커스텀훅 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 
-import instance from './apis/instance';
-import { ResponseDataType } from './common/Types';
+// import instance from './apis/instance';
+// import { DataAPI } from './apis/instance';
+// import { ResponseDataType } from './common/Types';
 import Chart from './components/Chart';
 import Filter from './components/Filter';
 import { ContextAPI } from './hooks/ContextAPI';
+import useResponse from './hooks/useResponse';
 
 function App() {
-  const [chartData, setChartData] = useState<ResponseDataType[]>();
-  const getInstance = async () => {
-    const data = await instance.get('');
-    const response: ResponseDataType[] = Object.values(data.data.response);
-    setChartData([...response]);
-  };
-  const [selectedId, setSelectedId] = useState<string>('');
+  const [chartData] = useResponse();
+  const [selectedId, setSelectedId] = useState<string>('전체');
+
   const handleButton = (target: string) => {
-    console.log(selectedId);
     setSelectedId(target);
   };
-
-  useEffect(() => {
-    getInstance();
-  }, []);
 
   if (chartData) {
     return (
