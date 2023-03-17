0d8b93a1bbaf3660ff51a433a37feec36745e3d2
whl5105 <73993670+whl5105@users.noreply.github.com>
2023-03-17 09:54:46 +0900

    [#2] Style : 메인 스타일 변경 , 코드 분리
    차트 데이터 커스텀 훅 분리 및 페이지 스타일 변경

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,66 +1,38 @@
-import { AxiosResponse } from 'axios';
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-import { chartApi } from '../apis/instance';
 import Filter from '../components/Filter';
-import MixedChart from '../components/MixedChart';
-import { IChart, IListResponse } from '../types/types';
-
-function sortDate(list: [string, IChart][]) {
-  const sorted_list = list.sort(function (a, b) {
-    return new Date(a[0]).getTime() - new Date(b[0]).getTime();
-  });
-  return sorted_list;
-}
+import TimeChart from '../components/TimeChart';
+import { useChartDataState } from '../hooks/useChartDataState';
 
 export default function Home() {
-  const [districts, setDistrict] = useState<string[]>([]);
-  const [date, setDate] = useState<string[]>([]);
-  const [chartData, setChartData] = useState<IChart[]>([]);
+  const {
+    isLoading,
+    dateValues,
+    areaValues,
+    barValues,
+    idValues,
+    districtValues,
+  } = useChartDataState();
 
-  const getChart = async () => {
-    const response: AxiosResponse<IListResponse> = await chartApi();
-    const data = response.data.response;
-
-    const chart = Object.entries(data);
-    setDate(sortDate(chart).map(date => date[0])); // 시간데이터
-    setChartData(sortDate(chart).map(date => date[1])); // 차트데이터
-
-    const district: string[] = ['전체'];
-    new Set(chart.map(x => x[1].id)).forEach(
-      el => !district.includes(el) && district.push(el)
-    );
-    setDistrict(district); // 지역구 데이터
-  };
-
-  useEffect(() => {
-    getChart();
-  }, []);
-
+  if (isLoading) return <Main>isLoading...</Main>;
   return (
     <Main>
       <Section>
-        <Title>Date Chart</Title>
+        <Title>Flexsys Time Series Chart</Title>
         <ChartContents className='chart-section'>
-          <Filter districts={districts} />
-          <MixedChart date={date} chartData={chartData} />
+          <Filter districtValues={districtValues} />
+          <TimeChart
+            dateValues={dateValues}
+            idValues={idValues}
+            areaValues={areaValues}
+            barValues={barValues}
+          />
         </ChartContents>
       </Section>
     </Main>
   );
 }
-
-const Main = styled.main`
-  width: 100vw;
-  height: 100vh;
-  padding: 20px 150px;
-  box-sizing: border-box;
-  background: linear-gradient(-45deg, #084464, #000101, #433abe);
-  background-size: 300% 300%;
-  animation: move-background 20s ease-in-out infinite;
-  color: #fff;
-  @keyframes move-background {
+const keyframesBg = keyframes`
     0% {
       background-position: 0% 50%;
     }
@@ -70,11 +42,25 @@ const Main = styled.main`
     100% {
       background-position: 0% 50%;
     }
-  }
+`;
+
+const Main = styled.main`
+  width: 100vw;
+  height: 100vh;
+  padding: 20px 150px;
+  box-sizing: border-box;
+  background: linear-gradient(
+    -45deg,
+    ${props => props.theme.bg.gradient_1},
+    ${props => props.theme.bg.gradient_2},
+    ${props => props.theme.bg.gradient_3}
+  );
+  background-size: 300% 300%;
+  animation: ${keyframesBg} 20s ease-in-out infinite;
+  color: ${props => props.theme.color.white};
+  text-align: center;
 `;
 const Section = styled.section`
-  border: 5px solid #e0e0e0;
-  border-radius: 17px;
   margin: 0 auto;
   padding: 20px 40px;
   box-sizing: border-box;
