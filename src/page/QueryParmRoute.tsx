3471c7e0b877938ccd6e3e0611837dcef37fed30
whl5105 <73993670+whl5105@users.noreply.github.com>
2023-03-17 09:59:19 +0900

    [#2] Docs : useSearchParams 파일명 수정

--- src/page/QueryParmRoute.tsx
+++ src/page/QueryParmRoute.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-import { useSearchParamsState } from '../hooks/useParams';
+import { useSearchParamsState } from '../hooks/useSearchParamsState';
 
 type RouteProps = {
   children: React.ReactElement;
